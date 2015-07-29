a56c1bde5ebfce7a49125ca9f1cd5eb8252fb89b
Darius Kasiulevičius <dariustri@gmail.com>
2015-07-29 23:48:20 +0300

    Updated robot logic

--- MiniSumoStraitght/MiniSumoStraitght.ino
+++ MiniSumoStraitght/MiniSumoStraitght.ino
@@ -2,7 +2,7 @@
 
 MiniRobot robot;
 
-byte status = 2;
+byte status;
 /*
 0 - don't need changes;
 1 - counter only
@@ -12,16 +12,17 @@ byte status = 2;
  - enemy forward;
 */
 
-int count = 0;
+unsigned long start_millis;
+long delay_millis;
+long dist;
 
 void setup() {
-  PCMSK1 |= bit(0) | bit(1);
+  pinMode(A0,INPUT);   // set Pin as Input (default)
+  pinMode(A1,INPUT);   // set Pin as Input (default)
+  PCMSK1 |= bit(0) + bit(1);
   PCIFR  |= bit(1);
   PCICR  |= bit(1);
-  pinMode(A0,INPUT);   // set Pin as Input (default)
-  digitalWrite(A0,HIGH);  // enable pullup resistor
-  pinMode(A1,INPUT);   // set Pin as Input (default)
-  digitalWrite(A1,HIGH);  // enable pullup resistor
+  status = 2;
 }
 
 ISR (PCINT1_vect)
@@ -32,16 +33,17 @@ ISR (PCINT1_vect)
 }
 
 void loop() {
+  Serial.println(status);
   switch (status) {
-    case 1:
-      count--;
-      if (count <= 0) {
+    case 1:      
+      if ((millis() - start_millis) >= delay_millis) {
         status = 3;
       }
       break;
     case 2:
       robot.back();
-      count = 15;
+      delay_millis = 600;
+      start_millis = millis();
       status = 1;
       break;
     case 3:
@@ -49,10 +51,17 @@ void loop() {
       status = 0;
       break;
     case 4:
-      robot.leftBack();
-      robot.rightForward();
-      count = 10;
-      status = 1;
+      robot.rightBack();
+      robot.leftForward();
+      delay_millis = 400;
+      start_millis = millis();
+      status = 5;
+      break;
+    case 5:
+      dist = robot.distanceToEnemy();
+      if (((millis() - start_millis) >= delay_millis) || (dist > 5)) {
+        status = 3;
+      }
       break;
   }
 }
